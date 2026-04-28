562616d38d753644bcd0e56b04925bdb471fff9b
Aslokn1a <asloknia@gmail.com>
2026-04-28 16:20:30 -0300

    Coisas extras adicionadas! exclusão de tarefa concludia, aviso de limite atingido e colocado limite nas tarefas concluidas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-function CriarTarefa(){
+function CreateTask(){
     
-    if(verLimite()){
+    if(CheckPending()){
 
 
     const valor = document.getElementById("tarefa").value;
@@ -8,7 +8,6 @@ function CriarTarefa(){
     
     if (valor.trim() === "") return;
 
-    
     const newDiv = document.createElement("div");
     newDiv.classList.add("d-flex", "justify-content-between", "p-3", "border", "mb-2","bg-white");
     newDiv.textContent = valor;
@@ -17,14 +16,29 @@ function CriarTarefa(){
     const btnConcluir = document.createElement("button");
     btnConcluir.textContent = "✔";
     btnConcluir.classList.add("btn", "btn-success", "btn-sm");
+    
     btnConcluir.onclick = function(){
+        if(CheckFinished()){
         newDiv.remove();
         const DivFinish = document.getElementById("Finish");
         const Finalize = document.createElement("div");
-        Finalize.classList.add("d-flex", "justify-content-between","border", "border-secondary", "p-3", "mb-2","bg-secondary-subtle");
-        Finalize.textContent = valor; 
+        const btnDelete = document.createElement("button");
+        btnDelete.textContent="X";
+        btnDelete.classList.add("btn", "btn-danger", "btn-sm")
         
+        btnDelete.onclick = function(){
+            Finalize.remove();
+            
+        }
+
+        Finalize.classList.add("d-flex", "justify-content-between","border", "border-secondary", "p-3", "mb-2","bg-success-subtle");
+        Finalize.textContent = valor; 
+        Finalize.appendChild(btnDelete)
         DivFinish.appendChild(Finalize);
+        document.getElementById("warning").remove();
+        } else{
+            Warning();
+        }
     };
 
 
@@ -41,15 +55,24 @@ function CriarTarefa(){
     document.getElementById("tarefa").value = "";
     
     } else{
-        console.log('Limite Atingido')
+        Warning();
     }
 };
 
 
-function verLimite(){
-    const limite =5;
+function CheckPending(){
+    const limite =60;
     const total = document.querySelectorAll('#Task > div').length;
-    console.log(total);
+    
+    if(total>=limite){
+        return false
+    } else{
+        return true
+    }
+};
+function CheckFinished(){
+    const limite =5;
+    const total = document.querySelectorAll('#Finish > div').length;
     
     if(total>=limite){
         return false
@@ -58,3 +81,19 @@ function verLimite(){
     }
 };
 
+function Warning(){
+    
+    const total = document.querySelectorAll('#warning').length;
+    console.log(total)
+
+    if(total<1){
+    const warn = document.createElement("div");
+    warn.classList.add("mx-auto", "p-3", "border","border-dark", "mb-2","bg-warning","row", "w-25", "d-flex","justify-content-center","rounded");
+    warn.textContent = "Limite Atingido!"
+    warn.id = "warning"
+    const div1 = document.getElementById("TaskSection")
+    div1.after(warn)
+    }else{
+        return;
+    }
+}
